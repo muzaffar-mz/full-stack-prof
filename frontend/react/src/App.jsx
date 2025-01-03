250274b8568811495c5b1f426ed895731f2ce72b
muzaffar-mz <m.muzaffar@atmos.uz>
2025-01-04 00:24:34 +0500

    Rendering Customers and Loading Spinner fully working

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,53 @@
+import { Button, Spinner, Text } from '@chakra-ui/react'
+import SidebarWithHeader from "./shared/SideBar.jsx";
+import { useEffect, useState } from 'react';
+import {getCustomers} from "./services/client.js";
+
+const App = () => {
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+
+        getCustomers().then(res => {
+            setCustomers(res.data);
+        }).catch(err => {
+            console.log(err);
+        }).finally(() => {
+            setLoading(false);
+        })
+    }, []);
+
+    if (loading) {
+        return (
+        <SidebarWithHeader>
+            <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
+            />
+        </SidebarWithHeader>
+        )
+    }
+
+    if (customers.length <= 0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+    }
+
+    return (
+        <SidebarWithHeader>
+            {customers.map((customer, index) => (
+                <p key={index}>{customer.name}</p>
+            ))}
+        </SidebarWithHeader>
+    )
+}
+
+export default App;
