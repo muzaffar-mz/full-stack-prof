e5d0cbe0a127d1fa26f0e8e0864e608d6aa4377b
muzaffar-mz <m.muzaffar@atmos.uz>
2025-01-24 15:25:53 +0500

    vite.config.js fix

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['http://full-stack-prof-env.eba-yxfcieth.ap-south-1.elasticbeanstalk.com'],
+  }
 })
