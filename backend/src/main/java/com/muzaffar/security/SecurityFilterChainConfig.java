93620b298c9ad57d362d614e888c6dc758636bb9
muzaffar-mz <m.muzaffar@atmos.uz>
2025-01-26 21:15:16 +0500

    Expose health and info metrics and add application-dev.yml

--- backend/src/main/java/com/muzaffar/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/muzaffar/security/SecurityFilterChainConfig.java
@@ -41,6 +41,8 @@ public class SecurityFilterChainConfig {
                                 .permitAll()
                                 .requestMatchers(HttpMethod.GET, "/ping")
                                 .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/actuator/**")
+                                .permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
