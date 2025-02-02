888e414eebefb280028138f20034fac7632fdf40
muzaffar-mz <m.muzaffar@atmos.uz>
2025-02-02 08:45:28 +0500

    New React routes

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -9,7 +9,7 @@ import {
     HStack,
     Icon,
     IconButton,
-    Image,
+    Image, Link,
     Menu,
     MenuButton,
     MenuDivider,
@@ -21,15 +21,20 @@ import {
     VStack
 } from '@chakra-ui/react';
 
-import {FiBell, FiChevronDown, FiCompass, FiHome, FiMenu, FiSettings, FiStar, FiTrendingUp} from 'react-icons/fi';
+import {
+    FiBell,
+    FiChevronDown,
+    FiHome,
+    FiMenu,
+    FiSettings,
+    FiUsers
+} from 'react-icons/fi';
 import {useAuth} from "../context/AuthContext.jsx";
 
 const LinkItems = [
-    {name: 'Home', icon: FiHome},
-    {name: 'Trending', icon: FiTrendingUp},
-    {name: 'Explore', icon: FiCompass},
-    {name: 'Favourites', icon: FiStar},
-    {name: 'Settings', icon: FiSettings},
+    {name: 'Home', route: '/dashboard', icon: FiHome},
+    {name: 'Customers', route: '/dashboard/customers', icon: FiUsers},
+    {name: 'Settings', route: '/dashboard/settings', icon: FiSettings},
 ];
 
 export default function SidebarWithHeader({children}) {
@@ -85,7 +90,7 @@ const SidebarContent = ({onClose, ...rest}) => {
                 <CloseButton display={{base: 'flex', md: 'none'}} onClick={onClose}/>
             </Flex>
             {LinkItems.map((link) => (
-                <NavItem key={link.name} icon={link.icon}>
+                <NavItem key={link.name} route={link.route} icon={link.icon}>
                     {link.name}
                 </NavItem>
             ))}
@@ -93,9 +98,9 @@ const SidebarContent = ({onClose, ...rest}) => {
     );
 };
 
-const NavItem = ({icon, children, ...rest}) => {
+const NavItem = ({icon, route, children, ...rest}) => {
     return (
-        // <Link href="frontend/react/src/components/shared#" style={{textDecoration: 'none'}} _focus={{boxShadow: 'none'}}>
+        <Link href={route} style={{textDecoration: 'none'}} _focus={{boxShadow: 'none'}}>
             <Flex
                 align="center"
                 p="4"
@@ -120,7 +125,7 @@ const NavItem = ({icon, children, ...rest}) => {
                 )}
                 {children}
             </Flex>
-        // </Link>
+        </Link>
     );
 };
 
